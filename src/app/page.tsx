74c0d1d2284036feb9a5752fbb2f6db1b3844fc6
Hy Ben <benhy3995@gmail.com>
2025-09-24 20:41:04 +0000

    nao ta dando scroll na pagina de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+    <main className="flex min-h-screen flex-col items-center justify-start p-4 pt-16 md:justify-center md:pt-4">
       <LoginForm />
     </main>
   );
