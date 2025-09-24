0388f5ff1f362ea2119bf98de8ff5d983aa82e9a
Hy Ben <benhy3995@gmail.com>
2025-09-24 21:23:05 +0000

    na pagina de login  A ROLAGEM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-4">
+    <main className="flex flex-col min-h-screen justify-center p-4">
       <LoginForm />
     </main>
   );
